96cb48fe889ebfa83977295a89f1d2c64a815220
kelayamatoz <zhaotian1991713@gmail.com>
2016-08-29 02:00:49 +0000

    rs232 working

--- pc_side/serial_read.py
+++ pc_side/serial_read.py
@@ -7,7 +7,8 @@ with serial.Serial(port='/dev/ttyUSB0',baudrate=230400,timeout=None) as ser:
     ser.flushOutput()
     start_time = time.time()
     for i in range(0, iter):
-        ser.read()
+        serial_line = ser.readline()
     end_time = time.time()
 
 print 'average ser read time = ', (end_time - start_time) / float(iter)
+print serial_line
